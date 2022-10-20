c8e793b5e8d350ef271a5de7a89bc2922d95d432
YAMADA Riko <lycopene109@outlook.jp>
2022-10-20 11:48:11 +0900

    あらかたできた

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'main.dart';
 import 'toppage.dart';
@@ -11,16 +13,97 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: TextButton(
-        child: Text('ログイン'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TopPage()),
-          );
-        },
-      )),
-    );
+        backgroundColor: Color.fromARGB(255, 255, 244, 228), // 背景色設定
+        body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight,
+              colors: [
+                Color.fromARGB(255, 255, 255, 255).withOpacity(0.6),
+                Color.fromARGB(255, 255, 226, 169).withOpacity(0.6),
+              ],
+              stops: const [
+                0.0,
+                1.0,
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('logo.png'),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 30),
+                child: Text('alarm',
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700])),
+              ),
+              Container(
+                  width: 275,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.topLeft,
+                      end: FractionalOffset.bottomRight,
+                      colors: [
+                        Color.fromARGB(255, 255, 143, 45).withOpacity(0.6),
+                        Color.fromARGB(255, 255, 119, 0).withOpacity(0.6),
+                      ],
+                      stops: const [
+                        0.0,
+                        1.0,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  margin: EdgeInsets.only(top: 50),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage("google_logo_white.png"),
+                        height: 25.0,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                        child: Text('Googleでログイン',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            )),
+                      )
+                    ],
+                  )),
+              Container(
+                  padding: EdgeInsets.only(top: 10),
+                  width: 275,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "アカウントを持っていませんか？",
+                        style: TextStyle(fontSize: 10, color: Colors.grey[700]),
+                      ),
+                      Text(
+                        '：サインイン',
+                        style: TextStyle(fontSize: 10, color: Colors.grey[700]),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
 }
